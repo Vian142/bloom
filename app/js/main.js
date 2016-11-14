3766b3164681661091ac93d2f5cae47a6123049a
Ivan Popov <vian142@gmail.com>
2016-11-14 04:03:10 +0300

    fix style, add jquery for menu, fix media style

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-
-
-        $('.services-block__item__image-services').imagesLoaded(function() {
-            console.log('all images are loaded');
-        }).imagefill();
-
-
-    $('.portfolio-block__item__image-work').imagesLoaded(function() {
+    $('.services-block__item__image-services').imagesLoaded(function () {
         console.log('all images are loaded');
     }).imagefill();
+    $('.portfolio-block__item__image-work').imagesLoaded(function () {
+        console.log('all images are loaded');
+    }).imagefill();
+
+
+    /* menu */
+    $('#main-menu-button').click(function () {
+        $(this).toggleClass('open');
+        $('#main-menu').toggleClass('main-menu--menu-show');
+    });
+    $('#main-menu').find('a').click(function () {
+        $('#main-menu-button').removeClass('open');
+        $('#main-menu').removeClass('main-menu--menu-show');
+    });
 
 });
